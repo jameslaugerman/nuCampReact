d2705a6eff8737f72d6f8531e1d74390103b7d50
James Laugerman <jameslaugerman@gmail.com>
2021-02-02 07:11:15 -0600

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-    // constructor(props) {
-    //     super(props);
-    // } 
-
-      renderCampsite(campsite){
+function RenderCampsite({campsite}) {
+   
           return(
               <div className="col-md-5 m-1">
                    <Card>
@@ -20,7 +16,7 @@ class CampsiteInfo extends Component {
           )
       }
 
-      renderComments(comments){
+      function RenderComments({comments}) {
         if(comments){
             return(
                 <div className="col-5-md m-1">
@@ -46,23 +42,18 @@ class CampsiteInfo extends Component {
         }
       }
 
-    render() {
-        if(this.props.campsite){
-            return(
-        <div className="row">
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
-        </div>
-            )
-        } 
-        else {
-            return(
-            <div>
-            
-            </div>
-            )
-        };
-    };
-}
-
-export default CampsiteInfo;
+      function CampsiteInfo(props) {
+        if (props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
+                    </div>
+                </div>
+            );
+        }
+        return <div />;
+    }
+    
+    export default CampsiteInfo;
